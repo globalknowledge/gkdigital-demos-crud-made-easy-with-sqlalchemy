582de83c1c692e9a536dd809cf1f6eda1b50b8f0
Michael Kennedy <mikeckennedy@gmail.com>
2016-09-30 13:43:24 -0400

    rounding out the types / tables.

--- core_style/program.py
+++ core_style/program.py
@@ -1,5 +1,7 @@
 import sqlalchemy
+from sqlalchemy import ForeignKey
 from sqlalchemy import Table, Column, Text, DateTime, Integer
+import datetime
 
 # 1. Create shared engine
 conn_str = 'sqlite:///./data/store.bin'
@@ -10,19 +12,20 @@ metadata = sqlalchemy.MetaData()
 
 # 3. Define schema
 customers = Table('customers', metadata,
-                  Column('id', Integer),
-                  Column('name', Text),
-                  Column('created', DateTime),
-                  Column('email', Text)
+                  Column('id', Integer, primary_key=True, autoincrement=True),
+                  Column('name', Text, nullable=False, unique=True, index=True),
+                  Column('created', DateTime, default=datetime.datetime.now),
+                  Column('email', Text, nullable=False, unique=True, index=True)
                   )
 
 addresses = Table('addresses', metadata,
-                  Column('id', Integer),
+                  Column('id', Integer, primary_key=True, autoincrement=True),
                   Column('street', Text),
                   Column('city', Text),
                   Column('state', Text),
                   Column('country', Text),
-                  Column('postal_code', Text)
+                  Column('postal_code', Text),
+                  Column('user_id', None, ForeignKey('customers.id'))
                   )
 
 # 4. Create the database
