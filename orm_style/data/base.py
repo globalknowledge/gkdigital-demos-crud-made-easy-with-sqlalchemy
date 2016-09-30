f6fda76c2f001b063522c74b7345f20b5ab1cf7d
Michael Kennedy <mikeckennedy@gmail.com>
2016-09-30 13:46:39 -0400

    inserting data

--- orm_style/data/base.py
+++ orm_style/data/base.py
@@ -1,8 +1,9 @@
 # noinspection PyUnresolvedReferences
 import sqlalchemy
+import sqlalchemy.orm
 
 # setup the engine
 conn_string = "sqlite:///./store_db.bin"
-engine = sqlalchemy.create_engine(conn_string)
-
+engine = sqlalchemy.create_engine(conn_string, echo=True)
+session_factory = sqlalchemy.orm.sessionmaker(engine)
 
--- orm_style/program.py
+++ orm_style/program.py
@@ -1,9 +1,33 @@
+import data.base
 import data.core
+from data.models import Customer
 
 
 def main():
     data.core.init()
+    insert_data()
     print('done')
+
+
+def insert_data():
+    first_customer = Customer()
+    first_customer.email = "jeff@jeff.com"
+    first_customer.name = "Jeff Rose"
+
+    print("First customer {} with id {} before save".format(
+        first_customer.name, first_customer.id
+    ))
+
+    uow = data.base.session_factory()
+
+    # make changes
+    uow.add(first_customer)
+
+    uow.commit()
+
+    print("First customer {} with id {} before save".format(
+        first_customer.name, first_customer.id
+    ))
 
 
 if __name__ == '__main__':
